82f7f52bf964c5ab7cf431ea19d069ce34eb4d78
Tiago Prysthon <tiagoprysthon14@gmail.com>
2024-05-20 09:33:03 -0300

    [feat] Locais - add new fields to register Locais

--- travel-planner-frontend/src/components/AddLocal.js
+++ travel-planner-frontend/src/components/AddLocal.js
@@ -1,21 +1,54 @@
-// components/AddLocal.js
 import React, { useState } from 'react';
 import api from '../services/api';
 import './AddLocal.css';
 import LocalList from './LocalList';
 
-const AddLocal = ({ onLocalAdded }) => { // Pass onLocalAdded prop
-  const [nome, setNome] = useState('');
+const AddLocal = ({ onLocalAdded }) => {
+  const [localData, setLocalData] = useState({
+    nome: '',
+    endereco: '',
+    preco: '',
+    horarioFuncionamento: '',
+    descricao: ''
+  });
+  const [precoError, setPrecoError] = useState('');
   const [needsRefresh, setNeedsRefresh] = useState(true);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "preco") {
+      if (validatePreco(value)) {
+        setPrecoError(''); // Limpar mensagem de erro se o preço é válido
+      } else {
+        setPrecoError('Por favor, insira um valor numérico válido com ponto decimal.');
+      }
+    }
+    setLocalData({ ...localData, [name]: value });
+  };
+
+  const validatePreco = (value) => {
+    // Regex para validar se o input é um número que pode incluir um ponto decimal
+    return /^[0-9]*\.?[0-9]*$/.test(value);
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (precoError) {
+      alert("Corrija os erros antes de enviar.");
+      return;
+    }
     try {
-      const {email} = JSON.parse(localStorage.getItem('user'));
-      await api.post('/locais', { nome, empresa: email, reserva: true }); // Use async/await for clarity
+      const { email } = JSON.parse(localStorage.getItem('user')); // Assume o email como nome da empresa
+      await api.post('/locais', { ...localData, empresa: email, reserva: true });
       console.log('Local adicionado!');
-      setNome(''); // Limpar o input após a adição
-      if (onLocalAdded) { // Call onLocalAdded if prop is provided
+      setLocalData({
+        nome: '',
+        endereco: '',
+        preco: '',
+        horarioFuncionamento: '',
+        descricao: ''
+      }); // Limpar o formulário após a adição
+      if (onLocalAdded) {
         onLocalAdded();
       }
     } catch (err) {
@@ -25,13 +58,42 @@ const AddLocal = ({ onLocalAdded }) => { // Pass onLocalAdded prop
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="add-local-form">
         <label htmlFor="nome">Nome do Local:</label>
         <input
           type="text"
-          id="nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
+          name="nome"
+          value={localData.nome}
+          onChange={handleChange}
+        />
+        <label htmlFor="endereco">Endereço:</label>
+        <input
+          type="text"
+          name="endereco"
+          value={localData.endereco}
+          onChange={handleChange}
+        />
+        <label htmlFor="preco">Preço:</label>
+        <input
+          type="text"
+          name="preco"
+          value={localData.preco}
+          onChange={handleChange}
+        />
+        {precoError && <div className="error">{precoError}</div>}
+        <label htmlFor="horarioFuncionamento">Horário de Funcionamento:</label>
+        <input
+          type="text"
+          name="horarioFuncionamento"
+          value={localData.horarioFuncionamento}
+          onChange={handleChange}
+        />
+        <label htmlFor="descricao">Descrição:</label>
+        <textarea
+          name="descricao"
+          value={localData.descricao}
+          onChange={handleChange}
+          rows="4"
         />
         <button type="submit">Adicionar Local</button>
       </form>
