e6fb89006ab5f05157643208eacc42e13e28f3c3
Tiago Prysthon <tiagoprysthon14@gmail.com>
2024-05-20 09:26:37 -0300

    [feat] Local Backend - add new fields

--- src/main/java/com/travelplanner/model/Local.java
+++ src/main/java/com/travelplanner/model/Local.java
@@ -1,10 +1,10 @@
-
 package com.travelplanner.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Column;
 
 @Entity
 public class Local {
@@ -15,6 +15,11 @@ public class Local {
     private String empresa;
     private boolean reserva;
     private String client;
+    private String endereco;  // Novo campo para o endereço do local
+    private double preco;     // Novo campo para preço
+    private String horarioFuncionamento; // Novo campo para horário de funcionamento
+    @Column(length = 1000)    // Aumentar o limite de caracteres para a descrição
+    private String descricao;  // Novo campo para descrição
 
     // Getters e Setters
     public Long getId() {
@@ -55,5 +60,37 @@ public class Local {
 
     public void setClient(String client) {
         this.client = client;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getHorarioFuncionamento() {
+        return horarioFuncionamento;
+    }
+
+    public void setHorarioFuncionamento(String horarioFuncionamento) {
+        this.horarioFuncionamento = horarioFuncionamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 }
